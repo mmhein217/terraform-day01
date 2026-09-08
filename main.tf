e70a563a10d9556f676d889fe13297f183abac74
Robin Myo <mmhein217@gmail.com>
2026-09-08 23:30:28 +0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}   
+
+data "aws_ami_ids" "exampleami" {
+  owners = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami_ids.exampleami.ids[0]
+  instance_type = "t2.medium"
+  tags = {
+    Name = "ExampleInstance"
+    Environment = "Development"
+  }
+  lifecycle {
+  create_before_destroy = true
+}
+}
